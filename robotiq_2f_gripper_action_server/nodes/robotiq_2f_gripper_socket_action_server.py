70754a71bf7599ba1d24434a7d74680e2761e8a7
Jack Lowry <jack.lowry@rutgers.edu>
2022-08-02 15:17:17 -0700

    Added support for changing gripper types

--- robotiq_2f_gripper_action_server/nodes/robotiq_2f_gripper_socket_action_server.py
+++ robotiq_2f_gripper_action_server/nodes/robotiq_2f_gripper_socket_action_server.py
@@ -5,24 +5,33 @@ import rospy
 import actionlib
 
 from control_msgs.msg import GripperCommandAction, GripperCommandGoal, GripperCommandResult, GripperCommandFeedback
-from robotiq_2f_gripper_control.msg import Robotiq2FGripper_robot_input, Robotiq2FGripper_robot_output
+# from robotiq_2f_gripper_control.msg import Robotiq2FGripper_robot_input, Robotiq2FGripper_robot_output
+from robotiq_2f_gripper_control.msg import finger_gripper_input, finger_gripper_output, vacuum_gripper_input, vacuum_gripper_output
 
 class GripperActionServer(object):
-    # create messages that are used to publish feedback/result
-    _status = Robotiq2FGripper_robot_input()
     _feedback = GripperCommandFeedback()
     _result = GripperCommandResult()
+    def __init__(self, name, gripper_type):
+        # create messages that are used to publish feedback/result
+        self.gripper_type = gripper_type
+        if(gripper_type == "vacuum"):
+            self.input_msg = vacuum_gripper_input
+            self.output_msg = vacuum_gripper_output
+        elif(gripper_type == "finger"):
+            self.input_msg = finger_gripper_input
+            self.output_msg = finger_gripper_output
+
+        self._status = self.input_msg()
 
 
-
-    def __init__(self, name):
         self._action_name = name
         self._as = actionlib.SimpleActionServer(self._action_name, GripperCommandAction, execute_cb=self.execute_cb, auto_start = False)
         self._as.start()
-        self.command_pub = rospy.Publisher("/gripper_control/command", Robotiq2FGripper_robot_output, queue_size=5)
-        self.status_listener = rospy.Subscriber("/gripper_control/status", Robotiq2FGripper_robot_input, queue_size=5, callback=self.update_status)
+        self.command_pub = rospy.Publisher("/gripper_control/command", self.output_msg, queue_size=5)
+        self.status_listener = rospy.Subscriber("/gripper_control/status", self.input_msg, queue_size=5,
+                                                callback=self.update_status)
 
-    def update_status(self, msg: Robotiq2FGripper_robot_input):
+    def update_status(self, msg):
         self._status = msg
       
     def execute_cb(self, goal: GripperCommandGoal):
@@ -32,15 +41,25 @@ class GripperActionServer(object):
         success = True
         
         # publish info to the console for the user
-        rospy.loginfo('%s: Moving gripper to %f with effort %f' % (self._action_name, goal.command.position, goal.command.max_effort))
-        command = Robotiq2FGripper_robot_output()
-        command.rPR = int(goal.command.position*255)
-        command.rSP = 100
-        command.rFR = int(goal.command.max_effort*255)
+        rospy.loginfo('%s: Moving gripper to %f with effort %f' % (
+        self._action_name, goal.command.position, goal.command.max_effort))
+        if self.gripper_type == "finger":
+            command = self.output_msg()
+            command.rPR = int(goal.command.position * 255)
+            command.rSP = 100
+            command.rFR = int(goal.command.max_effort * 255)
+        elif self.gripper_type == "vacuum":
+            command = self.output_msg()
+            print(goal.command.position)
+            if goal.command.position != 0:
+                command.rPR = 0
+            else:
+                command.rPR = 200
+        print(command)
         self.command_pub.publish(command)
 
-        #TODO(Jack, 6/21): Find a better way around the sleep to check when the gripper has actually recieved the command
-        rospy.sleep(.3)
+        # TODO(Jack, 6/21): Find a better way around the sleep to check when the gripper has actually recieved the command
+        rospy.sleep(1)
 
         # start executing the action
         while not rospy.is_shutdown():
@@ -61,11 +80,12 @@ class GripperActionServer(object):
           
         if success:
             self._result.position = self._feedback.position
-            self._result.reached_goal = False
+            self._result.reached_goal = success
             rospy.loginfo('%s: Succeeded' % self._action_name)
             self._as.set_succeeded(self._result)
         
 if __name__ == '__main__':
     rospy.init_node('gripper_action_server')
-    server = GripperActionServer(rospy.get_name())
+    gripper_type = rospy.get_param("~gripper_type", "finger")
+    server = GripperActionServer("gripper_controller/gripper_cmd", gripper_type)
     rospy.spin()
